9ee7d045f9d48c163c9d5c8bfbe9664ebccb08ea
myriam mansouri <myriam.mansouri@gmail.com>
2020-08-06 15:07:51 -0400

    Exercise 2: Understanding the initial state of the workshop

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,8 +1,14 @@
-import React from 'react';
-import { BrowserRouter as Router } from 'react-router-dom';
+import React from "react";
+import GlobalStyles from "../../GlobalStyles";
+import { BrowserRouter as Router } from "react-router-dom";
 
 const App = () => {
-  return <Router>TODO</Router>;
+  return (
+    <>
+      <Router>TODO</Router>
+      <GlobalStyles />
+    </>
+  );
 };
 
 export default App;
