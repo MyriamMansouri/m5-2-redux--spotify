082b0ea37606727781922d8f9211bda240a6d778
myriam mansouri <myriam.mansouri@gmail.com>
2020-08-06 16:44:45 -0400

    Exercise 4 - Frontend plumbing

---
+++ src/reducers/artists-reducer.js
@@ -0,0 +1,14 @@
+const initialState = {
+  currentArtist: null,
+  status: "idle",
+};
+
+const artistReducer = (state = initialState, action) => {
+  switch (action.type) {
+    default: {
+      return state;
+    }
+  }
+};
+
+export default artistReducer;
---
+++ src/reducers/auth-reducer.js
@@ -0,0 +1,11 @@
+const initialState = { token: null, status: "idle" };
+
+const authReducer = (state = initialState, action) => {
+  switch (action.type) {
+    default: {
+      return state;
+    }
+  }
+};
+
+export default authReducer;
